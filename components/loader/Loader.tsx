36085feaf86b2eac68c2474f81d19304c6595085
Kumar Piyush <kr.piyush888@gmail.com>
2024-08-01 12:06:06 +0530

    added toast

---
+++ components/loader/Loader.tsx
@@ -0,0 +1,10 @@
+// components/Loading.js
+import styles from './loader.module.css'
+const Loader = () => {
+    return (
+        <div className={`${styles.container}`}><div className = {`${styles.loaderLine}`}></div></div>
+    )
+};
+  
+  export default Loader;
+  
